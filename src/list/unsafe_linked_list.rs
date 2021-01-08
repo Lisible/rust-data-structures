69e6cd47a51bc2c19ab39280def3c82d2f1e055c
Clément Sibille <claymeuns@protonmail.com>
2021-01-08 21:01:39 +0100

    LinkedList: Add pop(&mut self)

--- src/list/unsafe_linked_list.rs
+++ src/list/unsafe_linked_list.rs
@@ -1,17 +1,18 @@
 use std::alloc::{handle_alloc_error, Allocator, Global, Layout};
 use std::marker::PhantomData;
 use std::ptr;
+use std::ptr::NonNull;
 
-pub struct LinkedList<T> {
-    head: *const Node<T>,
+pub struct LinkedList<T: Clone> {
+    head: *mut Node<T>,
     len: usize,
     _marker: PhantomData<T>,
 }
 
-impl<'a, T> LinkedList<T> {
+impl<'a, T: Clone> LinkedList<T> {
     pub fn new() -> Self {
         Self {
-            head: std::ptr::null() as *const Node<T>,
+            head: std::ptr::null::<Node<T>>() as *mut _,
             len: 0,
             _marker: PhantomData,
         }
@@ -43,7 +44,8 @@ impl<'a, T> LinkedList<T> {
             next: std::ptr::null::<*mut Node<T>>() as *mut Node<T>,
         };
 
-        // SAFETY: ptr is guaranteed to be not null, so we can write to it
+        // SAFETY: allocation has been successful, so ptr is guaranteed to be
+        // non-null, thus we can write to it
         unsafe {
             ptr::write::<Node<T>>(ptr.as_ptr() as *mut Node<T>, new_node);
 
@@ -63,10 +65,52 @@ impl<'a, T> LinkedList<T> {
         self.len += 1;
     }
 
+    pub fn pop(&mut self) -> Option<T> {
+        unsafe {
+            if self.len == 0 {
+                return None;
+            } else if self.len == 1 {
+                let result = Some((*self.head).value.clone());
+                self.len = 0;
+                let c: NonNull<Node<T>> = (&*self.head).into();
+                Global.deallocate(c.cast(), Layout::new::<Node<T>>());
+
+                return result;
+            }
+
+            let mut prev = self.head;
+            // SAFETY: self.head can't be null or we would have returned None
+            let mut current = (*self.head).next;
+
+            while (*current).next != ptr::null_mut() {
+                prev = current;
+
+                // SAFETY: current is not null or we would have exited the loop
+                current = (*current).next;
+            }
+
+            (*prev).next = std::ptr::null_mut();
+            let result = Some((*current).value.clone());
+            self.len -= 1;
+
+            let c: NonNull<Node<T>> = (&*current).into();
+            Global.deallocate(c.cast(), Layout::new::<Node<T>>());
+            result
+        }
+    }
+
     pub fn iter(&self) -> Iter<'a, T> {
         Iter {
             current: self.head,
             _marker: PhantomData,
+        }
+    }
+}
+
+impl<T: Clone> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        while self.len > 0 {
+            self.pop();
         }
     }
 }
@@ -117,6 +161,23 @@ mod tests {
 
         assert_eq!(linked_list.len(), 3);
         assert_eq!(linked_list.front(), Some(&5));
+    }
+
+    #[test]
+    pub fn linked_list_pop() {
+        let mut linked_list: LinkedList<i32> = LinkedList::new();
+        linked_list.push(5);
+        linked_list.push(12);
+        linked_list.push(23);
+
+        assert_eq!(linked_list.pop(), Some(23));
+        assert_eq!(linked_list.len(), 2);
+        assert_eq!(linked_list.pop(), Some(12));
+        assert_eq!(linked_list.len(), 1);
+        assert_eq!(linked_list.pop(), Some(5));
+        assert_eq!(linked_list.len(), 0);
+        assert_eq!(linked_list.pop(), None);
+        assert_eq!(linked_list.len(), 0);
     }
 
     #[test]
